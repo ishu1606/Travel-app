3fc70c3d414a36ad5a7663311ffe3953ebb0420a
Ishmeet Dhingra <ishmeetdhingra16@gmail.com>
2025-07-17 18:24:05 +0530

    2nd commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { BrowserRouter , Route  , Routes } from "react-router-dom"
+import Home from './pages/Home/Home'
+import Login from './pages/Auth/Login'
+import Signup from './pages/Auth/Signup'
 
 const App = () => {
   return (
-    <div className="text-3xl font-bold underline">App    </div>
+    <>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' exact element={<Home />} />
+      <Route path='/login' exact element={<Login />} />
+      <Route path='/sign-up' exact element={<Signup />} />
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
