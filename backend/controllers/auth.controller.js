43014786fbbedf1edb2da9c578a2271204b5b97b
Ishmeet Dhingra <ishmeetdhingra16@gmail.com>
2025-07-19 12:17:14 +0530

    create sign-in api route

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,30 +1,77 @@
-import bcryptjs from "bcryptjs";
-import User from "../models/user.model.js";
-import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs"
+import User from "../models/user.model.js"
+import { errorHandler } from "../utils/error.js"
+import jwt from "jsonwebtoken"
 
+export const signup = async (req, res, next) => {
+  const { username, email, password } = req.body
 
-export const signup = async(req , res , next) => {
-    const {username , email , password} = req.body 
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    return next(errorHandler(400, "All fields are required"))
+  }
 
-    if(!username || !email || !password || username === "" || email === "" || password === ""){
-        return next(errorHandler(400 , "All fields are required"))
+  // check if the user already exists
+  const existingUser = await User.findOne({ email })
+
+  if (existingUser) {
+    return next(errorHandler(409, "User already exist with this email!"))
+  }
+
+  const hashedPassword = bcryptjs.hashSync(password, 10)
+
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  })
+
+  try {
+    await newUser.save()
+
+    res.json("Signup successful")
+  } catch (error) {
+    next(error)
+  }
 }
-const hashedPassword = bcryptjs.hashSync(password ,10)
 
-const newUser =  new User({
-    username ,
-    email ,
-    password : hashedPassword,
-})
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body
 
-    try {
-        await newUser.save()
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "All fields are required"))
+  }
 
-        res.json("Signup Successful")
-    } catch (error) {
-        next(error)
+  try {
+    const validUser = await User.findOne({ email })
+
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"))
     }
 
-}
+    const validPassword = bcryptjs.compareSync(password, validUser.password)
 
-export default User
+    if (!validPassword) {
+      return next(errorHandler(400, "Wrong Credentials"))
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+
+    const { password: pass, ...rest } = validUser._doc
+
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest)
+  } catch (error) {
+    next(error)
+  }
+}
