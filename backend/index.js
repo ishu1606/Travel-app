abe5eaa47424d24432c7cbf624ab526543fbf3ad
Ishmeet Dhingra <ishmeetdhingra16@gmail.com>
2025-07-20 14:35:20 +0530

    Image uploading api route

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,12 @@ import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import userRoutes  from "./routes/user.route.js";
 import travelStoryRoutes from "./routes/travelStory.route.js"
+import { fileURLToPath } from "url";
 
 dotenv.config()
 
@@ -30,6 +32,12 @@ app.listen(3000 , () => {
 app.use("/api/auth" , authRoutes )
 app.use("/api/user" , userRoutes )
 app.use("/api/travel-story" , travelStoryRoutes )
+
+//server static files from the uploads and assests directory 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use("/uploads" , express.static(path.join(__dirname , "uploads")) )
 
 
 app.use((err , req , res , next) => {
