7dac21b7083bc0e542d0d79f2d25f8a2f1cff29d
Ishmeet Dhingra <ishmeetdhingra16@gmail.com>
2025-07-20 20:06:08 +0530

    delete image api route

--- backend/controllers/travelStory.controller.js
+++ backend/controllers/travelStory.controller.js
@@ -1,5 +1,9 @@
+import path from "path";
 import TravelStory from "../models/travelStory.model.js"
-import { errorHandler } from "../utils/error.js"
+import { errorHandler } from "../utils/error.js";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
 
 export const addTravelStory = async(req , res , next) => {
     const {title , story , visitedLocation , imageUrl , visitedDate} = req.body
@@ -60,6 +64,40 @@ export const imageUpload = async(req , res , next) => {
         const imageUrl = `http://localhost:3000/uploads/${req.file.filename}`
 
         res.status(201).json({imageUrl})
+    } catch (error) {
+        next(error)
+    }
+}
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const rootDir = path.join(__dirname , "..")
+
+export const deleteImage = async(req , res , next) => {
+    const {imageUrl} = req.query
+    
+    if(!imageUrl) {
+        return next(errorHandler(400 , "imageUrl parameter is require"))
+    }
+    try {
+        //extract the file name from imageUrl
+        const filename = path.basename(imageUrl)
+
+        //Delete the file path
+        const filePath = path.join(rootDir , "uploads" , filename)
+
+        console.log(filePath)
+
+        //check if the file exists
+        if(!fs.existsSync(filePath)) {
+            return next(errorHandler(404, "Image not found."))
+        }
+
+        // Delete the file
+        await fs.promises.unlink(filePath)
+
+        res.status(200).json({message : "Image deleted successfully!"})
     } catch (error) {
         next(error)
     }
